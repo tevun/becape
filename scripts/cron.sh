c3109182b7bccd49d58b54f591bd0ff423e72e11
William Correa <wilcorrea@gmail.com>
2019-07-08 23:12:15 -0300

    [feature] Add cron script

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -15,6 +15,9 @@ fi
 BECAPE_FILE_IDENTIFIER=$(date +"%Y_%m_%d_%H_%M_%S")
 source ${BECAPE_SCRIPT_DIR}/backup.sh
 
+BECAPE_UPLOAD_IDENTIFIER=${BECAPE_FILE_IDENTIFIER}
+source ${BECAPE_SCRIPT_DIR}/upload.sh
+
 echo " - "
 END=$(date +%s)
 TIME=$(( $END - $START ))
