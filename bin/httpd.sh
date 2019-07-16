40aae52d8b442cd04090cb13200e04bd644866d0
William Correa <wilcorrea@gmail.com>
2019-07-16 09:44:59 -0300

    [feature] Configure bin containers

--- bin/httpd.sh
+++ bin/httpd.sh
@@ -6,5 +6,7 @@ docker run -d \
   --restart always\
   -p 80:80\
   --name becape-httpd \
+  -v ${BECAPE_ROOT_DIR}/bin/httpd.conf:/usr/local/apache2/conf/httpd.conf \
+  -v ${BECAPE_ROOT_DIR}/bin/httpd.passwd:/usr/local/apache2/httpd.passwd \
   -v ${BECAPE_ROOT_DIR}/public:/usr/local/apache2/htdocs \
   httpd:2.4
